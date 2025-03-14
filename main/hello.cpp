c70479da5771892a4a1eeb01e8a364ec73a76698
Project1 <shobhaj.1023@gmail.com>
2025-03-14 12:36:52 +0530

    Updated hello.cpp for webhook testing

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,11 @@
-//File: hello.cpp
+// File: hello.cpp
 #include <iostream>
 using namespace std;
 
-int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+int main(void)
+{
+  cout << "Hello, CC\n";
+  cout << "Hello, PES1UG22AM074\n";
+  cout << "I have struggled to run this PES1UG22AM074  \n";
   return 0;
 }
